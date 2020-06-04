553c284ec8289d7bd54f0f43e7eb6028404d5c0f
Kunal Ghosh <kunalghosh.res@gmail.com>
2020-06-04 10:24:30 +0530

    feat: chaaneged maze color

--- src/map.cpp
+++ src/map.cpp
@@ -31,7 +31,7 @@ void Map::LoadMap() {
 // Render the map on screen on call
 
 void Map::RenderMap(SDL_Renderer* sdl_renderer, SDL_Rect* block) {
-    SDL_SetRenderDrawColor(sdl_renderer, 221, 221, 221, 1);
+    SDL_SetRenderDrawColor(sdl_renderer, 0x66, 0x66, 0x66, 0x01);
     for (auto const& points : _map) {
         block->x = points.x * block->w;
         block->y = points.y * block->h;
